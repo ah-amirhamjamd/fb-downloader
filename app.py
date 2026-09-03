2b4fe316d4487aa18befc0d2cbdc10f414c906fc
ah-amirhamjamd <amirhamjamd92@gmail.com>
2026-09-03 20:17:31 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import yt_dlp
+
+app = Flask(__name__)
+CORS(app) # ব্লগারের সাথে সংযোগের জন্য CORS চালু করা হলো
+
+@app.route('download', methods=['POST'])
+def download()
+    data = request.get_json()
+    url = data.get('url')
+    
+    if not url
+        return jsonify({'error' 'No URL provided'}), 400
+
+    try
+        ydl_opts = {
+            'format' 'best',
+            'quiet' True,
+            'no_warnings' True,
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl
+            info = ydl.extract_info(url, download=False)
+            download_url = info.get('url')
+            title = info.get('title', 'Facebook Video')
+            
+            return jsonify({
+                'success' True,
+                'title' title,
+                'download_url' download_url
+            })
+    except Exception as e
+        return jsonify({'success' False, 'error' str(e)}), 500
+
+if __name__ == '__main__'
+    app.run(host='0.0.0.0', port=5000)
